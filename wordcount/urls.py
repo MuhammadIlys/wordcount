85873783baeffb7ad47f01bc7cc4ff9d2bec58b9
root <root@localhost.localdomain>
2020-01-06 10:47:57 -0800

    changed homepage to home

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.homepage,name='home'),
+    path('', views.home,name='home'),
     path('contact/',views.contact,name='contact'),
     path('count/',views.count,name="countt")
     
